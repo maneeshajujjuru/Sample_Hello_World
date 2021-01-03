e3fdc3c78dca0e0223c8ee74f31d5b4c73d55349
Maneesha Jujjuru <Maneesha_Jujjuru@epam.com>
2021-01-03 10:19:50 +0530

    Add project files.

---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,26 @@
+using System.IO;
+using System;
+using Xunit;
+
+namespace TestProject
+{
+    public class UnitTest1
+    {
+
+        private const string Expected = "Hello World!";
+        [Fact]
+        public void Test1()
+        {
+
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                Sample.Program.Main();
+
+                var result = sw.ToString().Trim();
+                Assert.Equal(Expected, result);
+            }
+
+        }
+    }
+}
